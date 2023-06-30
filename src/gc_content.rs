297175cc84e368f6feafa533ed93f8bb041d16dc
mladen5000 <mladen5000@users.noreply.github.com>
2023-06-29 21:58:28 -0500

    Separate out and clean up the code for the Problem 4 -GC content

--- src/gc_content.rs
+++ src/gc_content.rs
@@ -1,39 +1,42 @@
 // Problem 4 - Computing the GC content of a DNA string
-use std::io::{BufRead, BufReader};
+use core::result::Result;
+use std::io::{BufRead, BufReader,Error};
+use std::fs::File;
+// use std::error::Error;
 
+//TODO - Implement function for each fasta sequence as opposed to per file
+//TODO - Or simply retain the max gc content and sequence
+fn computing_gc_content(filepath: &str) -> Result<f32, Error> {
+    let f = File::open(filepath)?;
+    let buffered = BufReader::new(f);
 
-fn computing_gc_content() {
-    // Problem 4 Computing GC Cntent
-    let my_path: &str = "/Users/mladenrasic/Downloads/rosalind_gc.txt";
-    let f = File::open(my_path)?;
-    let buffered  = BufReader::new(f);
+    // Initialize count variables
+    let mut gc_count = 0.0;
+    let mut total_count = 0.0;
 
     // Parse each line in fasta
     for line in buffered.lines() {
-        let line: String = line.expect("Unable to read line");
+        let line = line?;
 
-        // initialize gc and total
-
-        // Header line
+        // Skip header line
         if line.starts_with(">") {
             continue;
         }
 
-        //Calculate number of GC nucleotides
-        let gc_count: f32 = line.chars().filter(|c| (*c == 'C' || *c == 'G')).count() as f32;
-
-        // Calculate NT NUM
-        let total: usize = line.chars().count();
-
-        let gc_count: f32 = gc_count as f32;
-        let total: f32 = total as f32;
-        let gc: f32 = gc_count / total;
-
-        println!("GC Content: {}", gc);
-        //TODO - Return largest fasta
+        // Count GC content
+        gc_count = line.bytes().filter(|c| (*c == b'C' || *c == b'G')).count() as f32;
+        total_count = line.bytes().count() as f32;
     }
+
+    // Get GC fraction
+    let gc_content = gc_count / total_count;
+    Ok(gc_content)
+
+    //TODO - I think I need to return the sequence with largest gc content
 }
 
 fn main() {
-    computing_gc_content()
+    let gc_file = "/Users/mladenrasic/Downloads/rosalind_gc.txt";
+    let result = computing_gc_content(gc_file).expect("Error computing GC content");
+    println!("{result}")
 }
