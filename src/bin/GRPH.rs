47ff1df1a87e6fdcfea48aee54882e3f3d7f50db
mladen5000 <mladen5000@users.noreply.github.com>
2024-02-10 10:11:55 -0600

    Update GRPH.rs so that it is a MVP and remove blah.jl

--- src/bin/GRPH.rs
+++ src/bin/GRPH.rs
@@ -7,20 +7,22 @@ use std::io::{BufRead, BufReader};
 #[derive(Debug)]
 struct Node {
     id: String,
-    seq: Vec<u8>,
+    seq: String,
 }
 fn main() {
     let mut full_vec: Vec<Node> = vec![];
 
     // Parse the FASTQ file
-    let reader = BufReader::new(File::open("rosalind_grph.txt").expect("Failed to open file"));
+    let reader = BufReader::new(
+        File::open("/Users/mladenrasic/Downloads/sample.fastq").expect("Failed to open file"),
+    );
     let parser = fastq::Reader::new(reader);
 
     // Iterate over the records
     for record in parser.records() {
         let record = record.expect("Failed to parse record");
         let id = record.id().to_string();
-        let seq = record.seq().to_owned(); // Clone the sequence data
+        let seq = unsafe { String::from_utf8_unchecked(record.seq().to_owned()) }; // Clone the sequence data
 
         // Create a new node
         let end = record.id().len() - 3;
