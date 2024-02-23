9dd5bedafb4779f82a3876c778669a5ddd1e2f7e
mladen5000 <mladen5000@users.noreply.github.com>
2024-02-23 01:07:52 -0600

    Add consensus sequence calculation and matrix printing

--- src/bin/CONS.rs
+++ src/bin/CONS.rs
@@ -23,6 +23,7 @@ G: 1 1 6 3 0 1 0 0
 T: 1 5 0 0 0 1 1 6
 */
 
+use bio::bio_types::annot::pos;
 use bio::io::fasta;
 use std::fs::File;
 use std::io::BufReader;
@@ -34,42 +35,74 @@ fn main() {
         .map(fasta::Reader::new)
         .expect("Failed to open file and create reader");
 
+    let mut matrix = vec![];
+
     let (mut count_a, mut count_c, mut count_g, mut count_t) = (0, 0, 0, 0);
     // Go through the records and do boring stuff to get sequence
+
     for record in reader.records() {
         let record = record.expect("Failed to parse record");
-        let _id = record.id().to_string();
+        // let _id = record.id().to_string();
         let seq = unsafe { String::from_utf8_unchecked(record.seq().to_owned()) }; // Clone the sequence data
-        count_a = 0;
-        count_c = 0;
-        count_g = 0;
-        count_t = 0;
+        let seq_length = seq.len();
 
-        // Go through the chars
-        for c in seq.chars() {
+        // Populate consensus matrix
+        for (i, c) in seq.chars().enumerate() {
+            if matrix.len() < i + 1 {
+                matrix.push(vec![0; 4])
+            }
             match c {
-                'A' | 'a' => {
-                    count_a += 1;
-                }
-                'C' | 'c' => {
-                    count_c += 1;
-                }
-                'G' | 'g' => {
-                    count_g += 1;
-                }
-                'T' | 't' => {
-                    count_t += 1;
-                }
-                _ => todo!(),
+                'A' | 'a' => matrix[i][0] += 1,
+                'C' | 'c' => matrix[i][1] += 1,
+                'G' | 'g' => matrix[i][2] += 1,
+                'T' | 't' => matrix[i][3] += 1,
+                _ => panic!("AHhhhhhhHHHHh"),
             }
         }
-        println!("A: {}", count_a);
-        println!("C: {}", count_c);
-        println!("G: {}", count_g);
-        println!("T: {}", count_t);
+    }
+
+    let consensus_seq = get_consensus_sequence(&matrix);
+    println!("{}", consensus_seq);
+    // Output consense matrix
+    print_consensus_matrix(&matrix);
+    println!();
+
+    // println!("{:?}", matrix);
+}
+
+fn get_consensus_sequence(matrix: &Vec<Vec<i32>>) -> String {
+    // Get the index where the max occurs for each col
+    let consensus_index: Vec<_> = matrix
+        .iter()
+        .map(|row| {
+            row.iter()
+                .enumerate()
+                .max_by_key(|&(_, item)| item)
+                .unwrap()
+                .0
+        })
+        .collect();
+    // print!("{:?} ", consensus_index);
+
+    let consensus_seq: String = consensus_index
+        .iter()
+        .map(|i| match i {
+            0 => 'A',
+            1 => 'C',
+            2 => 'G',
+            3 => 'T',
+            _ => unreachable!(),
+        })
+        .collect();
+    // println!("{:?}", consensus_seq);
+    consensus_seq
+}
+
+fn print_consensus_matrix(matrix: &Vec<Vec<i32>>) {
+    for c in 0..matrix[0].len() {
+        for r in 0..matrix.len() {
+            print!("{} ", matrix[r][c]);
+        }
         println!();
-        // Need to get count a at position 1, position 2, position 3, etc.
-        // Then get the max of each position
-        // Then I can do the same for each letter
     }
 }
